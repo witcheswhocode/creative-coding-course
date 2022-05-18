1c0c1ce044445ccb5abf7f415a49e6bed5397c50
liz <lizanderson.ca@gmail.com>
2022-05-18 14:57:34 -0700

    start lesson 4

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -25,6 +25,12 @@ const sketch = ({ context, width, height }) => {
       for (let j = i + 1; j < agents.length; j++){
         const other = agents[j];
 
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
         context.beginPath();
         context.moveTo(agent.pos.x, agent.pos.y);
         context.lineTo(other.pos.x, other.pos.y);
@@ -46,6 +52,11 @@ class Vector {
   constructor(x,y){
     this.x = x;
     this.y = y;
+  }
+  getDistance(v){
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx*dx + dy*dy); //pythag
   }
 }
 class Agent{

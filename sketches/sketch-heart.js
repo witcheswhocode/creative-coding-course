800bce42ee46644a71992f3628e97ad8e139c86b
liz <lizanderson.ca@gmail.com>
2022-06-08 22:38:21 -0700

    heart on fire

--- sketches/sketch-heart.js
+++ sketches/sketch-heart.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 const tweakpane = require('tweakpane');
 
 const settings = {
@@ -6,9 +7,33 @@ const settings = {
 };
 
 const params = {
-  hwidth: 50,
-  hheight: 50,
+  hwidth: 500,
+  hheight: 500,
 };
+
+function getXDimension(layer){
+  switch(layer){
+    case 0:
+      return 0.5
+    case 1:
+      return 0.45
+    case 2:
+      return 0.35
+    case 3:
+      return 0.30
+    case 4:
+      return 0.20
+    case 5:
+      return 0.10
+    case 6:
+      return 0.05
+  }
+}
+function getRandomColor(){
+  const randomNum = random.rangeFloor(0,2);
+  const colors = ["rgba(255, 0, 0, 0.1)","rgba(255, 265, 0, 0.1)","rgba(255, 237, 71, 0.1)"];
+  return colors[randomNum];
+}
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -16,46 +41,97 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
     var x = width/2;
     var y = height/2;
-    var hwidth = params.hwidth;
-    var hheight = params.hheight;
+    var hwidth = 500;
+    var hheight = 500;
+    var topCurveHeight = hheight * 0.3;
   
     context.save();
     context.beginPath();
-    var topCurveHeight = hheight * 0.3;
     context.moveTo(x, y + topCurveHeight);
 
-    // top right curve
+    // top left curve
     context.bezierCurveTo(
       x, y, 
-      x + hwidth / 2, y, 
-      x + hwidth / 2, y + topCurveHeight
+      x - hwidth / 2, y, 
+      x - hwidth / 2, y + topCurveHeight
     );
 
-    // bottom right curve
+    // bottom left curve
     context.bezierCurveTo(
-      x + hwidth / 2, y + (hheight + topCurveHeight) / 2, 
+      x - hwidth / 2, y + (hheight + topCurveHeight) / 2, 
       x, y + (hheight + topCurveHeight) / 2, 
       x, y + hheight
     );
 
     // bottom right curve
-    /*context.bezierCurveTo(
+    context.bezierCurveTo(
       x, y + (hheight + topCurveHeight) / 2, 
       x + hwidth / 2, y + (hheight + topCurveHeight) / 2, 
-      x + hwidth / 2, y + topCurveHeight);
-    
+      x + hwidth / 2, y + topCurveHeight
+    );
+
     // top right curve
     context.bezierCurveTo(
       x + hwidth / 2, y, 
       x, y, 
       x, y + topCurveHeight
-    );*/
+    );
 
 
     context.closePath();
     context.fillStyle = 'red';
     context.fill();
     context.restore();
+
+    let particles = []
+    for (let j = 0; j < 7; j++){
+      const inc = 23*j;
+      console.log(inc);
+      for (let i = inc; i < 150; i++){
+        const xs = x-hwidth*(getXDimension(j));
+        const xe = x+hwidth*(getXDimension(j));
+        const ys = y + topCurveHeight*(0.6-(j*0.3));
+        const ye = y + topCurveHeight*(1.2-(j*0.3));
+        context.save();
+        context.beginPath();
+        context.arc(random.range(xs,xe), random.range(ys,ye), 40, 0, 2 * Math.PI);
+        context.fillStyle = getRandomColor();
+
+        context.fill();
+        context.restore();
+      }
+
+    }
+
+
+    /*context.save();
+    context.beginPath();
+    context.arc(x-hwidth*0.2, y + topCurveHeight*-0.5, 20, 0, 2 * Math.PI);
+    context.arc(x-hwidth*0.3, y + topCurveHeight*-0.3, 20, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.restore();
+    context.save();
+    context.beginPath();
+    context.arc(x-hwidth*0.4, y + topCurveHeight*-0.1, 20, 0, 2 * Math.PI);
+    context.arc(x-hwidth*0.45, y + topCurveHeight*0.2, 20, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.restore();
+    context.save();
+    context.beginPath();
+    context.arc(x-hwidth*0.45, y + topCurveHeight*0.5, 20, 0, 2 * Math.PI);
+    context.arc(x-hwidth*0.5, y + topCurveHeight*0.8, 20, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.restore();
+    context.save();
+    context.beginPath();
+    context.arc(x-hwidth*0.5, y + topCurveHeight*1.2, 20, 0, 2 * Math.PI);
+    context.arc(x-hwidth*0.45, y + topCurveHeight*1.5, 20, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.restore();*/
     
   };
 };
@@ -65,8 +141,8 @@ function createPane(){
   let folder;
 
   folder = pane.addFolder({title: 'Heart'});
-  folder.addInput(params, 'hwidth', {min:2,max:50,step:1});
-  folder.addInput(params, 'hheight', {min:2,max:50,step:1});
+  folder.addInput(params, 'hwidth', {min:500,max:1000,step:1});
+  folder.addInput(params, 'hheight', {min:500,max:1000,step:1});
 }
 
 createPane();

08a9efebec05876963733f00d3afb40b35874bca
liz <lizanderson.ca@gmail.com>
2022-06-08 23:53:11 -0700

    hearts colliding and dying

--- sketches/sketch-hearts.js
+++ sketches/sketch-hearts.js
@@ -10,7 +10,7 @@ const settings = {
 const sketch = ({ context, width, height }) => {
   const agents = [];
 
-  for(let i = 0; i < 50; i++){
+  for(let i = 0; i < 30; i++){
     const x = random.range(0,width);
     const y = random.range(0,height);
     agents.push(new Heart(x,y));
@@ -60,6 +60,7 @@ class Heart{
     this.pos = new Vector(x,y);
     this.velocity = new Vector(random.range(-1,1),random.range(-1,1));
     this.half = (random.range(0,1) > 0.5) ? 'left' :  'right';
+    this.colorLevel = 0;
   }
 
   update(){
@@ -82,6 +83,8 @@ class Heart{
     var width = 200;
     var height = 200;
     
+    // kill heart after it burns
+    if (this.colorLevel > 10) return;
     // skip dead heart that became whole
     if (this.half == 'none') return;
 
@@ -149,13 +152,95 @@ class Heart{
     }
     
     if (this.half == 'whole') {
-      context.fillStyle = 'black';
+      if (this.colorLevel < 8){
+        context.fillStyle = this.getHeartColor(this.colorLevel);
+      }
+      else {
+        context.fillStyle = this.getHeartColor(10);
+      }
+      this.colorLevel += 0.05;
+      //this.drawFire();
     }
     else {
-      context.fillStyle = 'red';
+      context.fillStyle = this.getHeartColor(0);
     }
     context.fill();
     context.restore();
+
+    if (this.half == 'whole') {
+      this.drawFire(context);
+    }
   
+  }
+  getXDimension(layer){
+    switch(layer){
+      case 0:
+        return 0.5
+      case 1:
+        return 0.45
+      case 2:
+        return 0.35
+      case 3:
+        return 0.30
+      case 4:
+        return 0.20
+      case 5:
+        return 0.10
+      case 6:
+        return 0.05
+    }
+  }
+  getHeartColor(layer){
+    /*if(3 <= layer < 4.5){
+      console.log('yes');
+      return "rgb(164, 22, 26)"
+    }*/
+    if (layer < 1){
+      return "rgb(229, 56, 59)";
+    }
+    else if (layer < 2){
+      return "rgb(186, 24, 27)";
+    }
+    else if (layer < 4){
+      return "rgb(164, 22, 26)";
+    }
+    else if (layer < 6){
+      return "rgb(102, 7, 8)";
+    }
+    else if (layer < 8){
+      return "rgb(22, 26, 29)";
+    }
+    else if (layer >= 8){
+      return "rgb(11, 9, 10)";
+    }
+  }
+  getRandomColor(){
+    const randomNum = random.rangeFloor(0,2);
+    const colors = ["rgba(255, 0, 0, 0.1)","rgba(255, 265, 0, 0.1)","rgba(255, 237, 71, 0.1)"];
+    return colors[randomNum];
+  }
+
+  drawFire(context){
+    var x = this.pos.x;
+    var y = this.pos.y;
+    var hwidth = 200;
+    var hheight = 200;
+    var topCurveHeight = hheight * 0.3;
+    for (let j = 0; j < 7; j++){
+      const inc = 10*j;
+      for (let i = inc; i < 100; i++){
+        const xs = x-hwidth*(this.getXDimension(j));
+        const xe = x+hwidth*(this.getXDimension(j));
+        const ys = y + topCurveHeight*(0.6-(j*0.3));
+        const ye = y + topCurveHeight*(1.2-(j*0.3));
+        context.save();
+        context.beginPath();
+        context.arc(random.range(xs,xe), random.range(ys,ye), 20, 0, 2 * Math.PI);
+        context.fillStyle = this.getRandomColor();
+
+        context.fill();
+        context.restore();
+      }
+    }
   }
 }
